c1b55525df5c95adbae8a33cc178c2a3c4a02be7
Miguel <marsigliamiguel@protonmail.com>
2019-06-28 16:52:59 -0600

    DAOImpl class fleshed-out, Actor/Film classes added

--- src/main/java/com/skilldstillery/film/dao/FilmDAO.java
+++ src/main/java/com/skilldstillery/film/dao/FilmDAO.java
@@ -1,5 +1,15 @@
 package com.skilldstillery.film.dao;
 
-public class FilmDAO {
+import java.sql.SQLException;
+import java.util.List;
+
+import com.skilldistillery.film.entities.*;
+
+
+public interface FilmDAO {
+	
+	public Film findFilmById(Integer filmId) throws SQLException;
+	public Film findFilmByKeyword(String keyword) throws SQLException;
+	public List<Actor> findActorsByFilmId(int filmId);
 
 }
